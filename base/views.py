ddb6a17abd23ad7330f5a98a330c5b5fc49e8f27
Brian Connolly <Brian.connolly71@gmail.com>
2021-12-02 16:13:52 +0000

    added view and model setup

--- base/views.py
+++ base/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 
 # Create your views here.
+
+def scrape(request):
+    page = requests.get('https://www.facebook.com')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    
+    link_address = []
+    
+    for x in soup.find_all('a'):
+        link_address.append(x.get('href'))
+    
+    template = 'base/result.html'
+    context = {
+        'link_address': link_address
+    }
+    
+    return render(request, template, context)
