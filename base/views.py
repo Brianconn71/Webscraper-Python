fd4d46c71da714ab0f875dbbcf154441861b9333
Brian Connolly <Brian.connolly71@gmail.com>
2021-12-03 20:02:24 +0000

    add: added bootstrap table with link info

--- base/views.py
+++ base/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
+from .models import Link
 
 # Create your views here.
 
 def scrape(request):
-    page = requests.get('https://www.facebook.com')
+    page = requests.get('https://www.google.com')
     soup = BeautifulSoup(page.text, 'html.parser')
     
-    link_address = []
-    
     for x in soup.find_all('a'):
-        link_address.append(x.get('href'))
+        link_address = x.get('href')
+        link_text = x.string
+        Link.objects.create(address=link_address, name=link_text)
+    
+    data = Link.objects.all()
     
     template = 'base/result.html'
     context = {
-        'link_address': link_address
+        'data': data,
     }
     
     return render(request, template, context)
