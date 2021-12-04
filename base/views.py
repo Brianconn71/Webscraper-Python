1c5de6f5e7bec2668c7fd73eca87b45bdb4e6e17
Brian Connolly <Brian.connolly71@gmail.com>
2021-12-04 11:35:24 +0000

    add: added search bar to search for site

--- base/views.py
+++ base/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
+from django.http import HttpResponseRedirect
 from .models import Link
 
 # Create your views here.
 
 def scrape(request):
-    page = requests.get('https://www.google.com')
-    soup = BeautifulSoup(page.text, 'html.parser')
+    if request.method == "POST":
+        site = request.POST.get('site')
+        page = requests.get(site)
+        soup = BeautifulSoup(page.text, 'html.parser')
     
-    for x in soup.find_all('a'):
-        link_address = x.get('href')
-        link_text = x.string
-        Link.objects.create(address=link_address, name=link_text)
+        for x in soup.find_all('a'):
+            link_address = x.get('href')
+            link_text = x.string
+            Link.objects.create(address=link_address, name=link_text)
+        return 
     
     data = Link.objects.all()
     
